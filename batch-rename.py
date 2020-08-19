2f621ebf8d5e35f572dd5c8bd1ba6b677f09cf0d
Derrick Mateja <dmateja724@gmail.com>
2020-08-19 18:06:39 -0500

    files get put into folders after rename - working on moving them

--- batch-rename.py
+++ batch-rename.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import shutil
 
 # TODO: for ease of use we'll want to create folders appropriate to the dates
 # that the videos were created on for easy orgranization
@@ -11,7 +12,7 @@ import datetime
 now = datetime.datetime.now()
 user_name = 'dmate'
 dir = f'C:\\Users\\{user_name}\\Videos\\Captures'
-move_dir = ' '
+destination_dir = f'C:\\Users\{user_name}\\Nextcloud\\COD Captures'
 
 
 # this function will check the string passed in if each character is an
@@ -26,6 +27,9 @@ def create_new_folder_check(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+def move_files(path, destination_path):
+    shutil.move(path, destination_path)
+
 with os.scandir(dir) as dir_entries:
     for entry in dir_entries:
         path = entry.path
@@ -37,15 +41,16 @@ with os.scandir(dir) as dir_entries:
             new_file_name = file_split[5] + ' ' + file_split[6]
             folder_name = file_split[5].split(f'{now.year}-')[1]
             folder_path = os.path.join(dir, folder_name)
-            # destination_path = os.path.join(dir, folder_name, new_file_name)
+            destination_path = os.path.join(dir, folder_name, new_file_name)
 
 
             create_new_folder_check(folder_path)
 
             try:
-               rename_files(path, destination_path)
+                rename_files(path, destination_path)
             except Exception as err:
                 print(err)
-
         else:
             pass
+    # only moves the last folder in, its outside the loop so it only does the last one
+    move_files(folder_path, destination_dir)
